24a1e853c5f0db13eb9951a6409dcac02ebdabd8
LUIS316l <luiseduardoruizalvidrez316@gmail.com>
2024-05-18 22:58:59 -0600

    Evaluendo una formula gigante en python y cpp

---
+++ Python/EvaluandoUnaFormulaGigante.py
@@ -0,0 +1,7 @@
+import math
+
+x, y, z = map(float, input().split())
+
+operacion = (((2 * x + y) / z) * (math.pow(y, 3) - z)) / ((x + 2 * y + 3 * z) / (z - 2 * y - 3 * x) + math.pow(x, 2) + math.pow(z, 2))
+
+print(operacion)
