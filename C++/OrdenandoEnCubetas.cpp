7a820b4bc9bc9db8625c6c1c30e06bd15c30ba67
LUIS316l <luiseduardoruizalvidrez316@gmail.com>
2024-08-16 22:44:02 -0600

    Modificacion de un problema

--- C++/OrdenandoEnCubetas.cpp
+++ C++/OrdenandoEnCubetas.cpp
@@ -6,8 +6,10 @@ using namespace std;
 int main()
 {
     int cantidadDePelotas, maximoNumeroAEncontrar, numPelotas;
+
     cin >> cantidadDePelotas;
     cin >> maximoNumeroAEncontrar;
+    
     vector<int> pelotas(cantidadDePelotas + 1);
 
     if (maximoNumeroAEncontrar < 101)
